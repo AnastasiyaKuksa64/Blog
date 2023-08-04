029419c1eee62b15b166b6e8a93cbdd8b30619eb
AnastasiyaKuksa64 <kuksa.nastya649@gmail.com>
2023-08-04 23:54:34 +0300

    added new functionality for the post

--- src/components/BigCard/index.tsx
+++ src/components/BigCard/index.tsx
@@ -1,15 +1,15 @@
 import styles from "./cardList.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import {
-  faTrash,
-  faBookmark,
-  faEllipsis,
-} from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faBookmark } from "@fortawesome/free-solid-svg-icons";
 import { faFacebookF, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import { addFavourite, delitemarksFavourite } from "../../appSlices/postsSlice";
-import { useNavigate } from "react-router-dom";
+import {
+  addFavourite,
+  delitemarksFavourite,
+  selectPosts,
+} from "../../appSlices/postsSlice";
+import { useNavigate, Link } from "react-router-dom";
 
 interface IBigCardProps {
   date: string;
@@ -24,25 +24,30 @@ const BigCard: React.FC<IBigCardProps> = (props) => {
   const faFacebooIcon = faFacebookF as IconProp;
   const { title, discription, src, id } = props;
 
-  const bookMarks = useAppSelector((state) => state.posts.favourites);
+  const { favourites: bookMarks } = useAppSelector(selectPosts);
 
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const handleForward = () => {
     navigate(-1);
   };
+  const onClickRemove = () => {
+    if (window.confirm("Are you sure ?")) {
+      dispatch(delitemarksFavourite(id));
+    }
+  };
   return (
     <>
       <div className={styles.post}>
-        <span className="linkToHome" onClick={handleForward}>
-          Home
+        <span className="link_home" onClick={handleForward}>
+          Back to Home /
         </span>
-        <div className={styles.card_content_wrap}>
-          <h3 className="CardTitle">{title}</h3>
+        <div className={styles.card}>
+          <h3 className="card_title">{title}</h3>
           <div className={styles.card_img}>
             <img src={src} alt={"src"} />
           </div>
-          <div className={styles.wrap_content}>
+          <div className={styles.card_description}>
             <div className="card_text">
               <p>{discription}</p>
               <p>{discription}</p>
@@ -52,33 +57,28 @@ const BigCard: React.FC<IBigCardProps> = (props) => {
             <div className={styles.card_footer}>
               <ul className="marks">
                 <li className="icon">
-                  <FontAwesomeIcon icon={faTwitterIcon} />
+                  <Link to="https://twitter.com/">
+                    <FontAwesomeIcon icon={faTwitterIcon} />
+                  </Link>
                 </li>
                 <li className="icon">
-                  <FontAwesomeIcon icon={faFacebooIcon} />
-                </li>
-                <li className="icon">
-                  <FontAwesomeIcon icon={faEllipsis} />
+                  <Link to="https://ru-ru.facebook.com/">
+                    <FontAwesomeIcon icon={faFacebooIcon} />
+                  </Link>
                 </li>
               </ul>
-              <div className="icon">
-                <>
-                  {bookMarks.find((item) => item.id === id) ? (
-                    <>
-                      <div onClick={() => dispatch(delitemarksFavourite(id))}>
-                        <FontAwesomeIcon icon={faTrash} />
-                      </div>
-                    </>
-                  ) : (
-                    <>
-                      <div onClick={() => dispatch(addFavourite(props))}>
-                        <FontAwesomeIcon icon={faBookmark} />
-                      </div>
-                      <></>
-                    </>
-                  )}
-                </>
-              </div>
+              {bookMarks.find((item: { id: string }) => item.id === id) ? (
+                <div className="icon" onClick={onClickRemove}>
+                  <FontAwesomeIcon icon={faTrash} />
+                </div>
+              ) : (
+                <div
+                  className="icon"
+                  onClick={() => dispatch(addFavourite(props))}
+                >
+                  <FontAwesomeIcon icon={faBookmark} />
+                </div>
+              )}
             </div>
           </div>
         </div>
