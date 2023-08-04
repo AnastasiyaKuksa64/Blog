6455e04add5ab0b4e733838b6df76f8ce3d8ef27
AnastasiyaKuksa64 <kuksa.nastya649@gmail.com>
2023-08-05 00:48:07 +0300

    changed API

--- src/appSlices/postsSlice.ts
+++ src/appSlices/postsSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
 import { getItem } from "../utils/localStorage";
+import axios from "axios";
 
 type Post = {
   date: string;
@@ -11,30 +12,44 @@ type Post = {
   id: string;
 };
 
+export type Sort = {
+  name: string;
+  sortProperty: string;
+};
+
 type PostsState = {
   posts: Post[];
   favourites: Post[];
   post: null | Post;
-  tabs: object | any;
+  status: string;
+};
+
+type fetchType = {
+  categoryId: number;
+  sort: Sort;
+  currentPage: number;
+  search: string;
 };
 
 const initialState: PostsState = {
   posts: [],
   favourites: getItem("favoutites") || [],
   post: null,
-  tabs: { all: "All", myFavorites: "My Favorites" },
+  status: "loading", // loading, success, error
 };
 
 export const fetchPost = createAsyncThunk(
   "posts/Posts",
-  async (_, { rejectWithValue }) => {
-    const response = await fetch(`http://localhost:3000/posts`);
-
-    if (!response.ok) {
-      return rejectWithValue("Server error");
-    }
-    const json = await response.json();
-    return json;
+  async (parameters: fetchType, thunkAPI) => {
+    const { categoryId, search, sort, currentPage } = parameters;
+    const { data } = await axios.get(
+      `http://localhost:3000/posts?_page=${currentPage}}&_limit=6&${
+        categoryId > 0 ? `category=${categoryId}` : ""
+      }&_sort=${sort.sortProperty.replace("-", "")}&_order=${
+        sort.sortProperty.includes("-") ? "asc" : "desc"
+      }${search}`
+    );
+    return data;
   }
 );
 
@@ -44,7 +59,7 @@ export const fetchPostById = createAsyncThunk(
     const response = await fetch(`http://localhost:3000/posts/${id}`);
 
     if (!response.ok) {
-      return rejectWithValue("error poen post");
+      return rejectWithValue("error open post");
     }
 
     const json = await response.json();
@@ -53,7 +68,7 @@ export const fetchPostById = createAsyncThunk(
 );
 
 export const postsSlice = createSlice({
-  name: "posts", //todos
+  name: "posts",
   initialState,
   reducers: {
     addFavourite: (state, action: PayloadAction<any>) => {
@@ -69,13 +84,41 @@ export const postsSlice = createSlice({
       let tostring = JSON.stringify(state.favourites);
       localStorage.setItem("favoutites", tostring);
     },
+    deliteAllFavourite: (state, action: PayloadAction<any>) => {
+      state.favourites = [];
+      let tostring = JSON.stringify(state.favourites);
+      localStorage.setItem("favoutites", tostring);
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(fetchPostById.fulfilled, (state, action) => {
       state.post = action.payload;
     });
+    builder.addCase(fetchPostById.rejected, (state, action) => {
+      state.status = "error";
+    });
+    builder.addCase(fetchPostById.pending, (state, action) => {
+      state.status = "pending";
+    });
+    //[fetchPost.fulfilled]:(state, action) =>{
+    // }
+    builder.addCase(fetchPost.pending, (state, action) => {
+      state.status = "loading";
+      state.posts = [];
+    });
+    builder.addCase(fetchPost.fulfilled, (state, action) => {
+      state.posts = action.payload;
+      state.status = "success";
+    });
+    builder.addCase(fetchPost.rejected, (state, action) => {
+      state.status = "error";
+      state.posts = [];
+    });
   },
 });
 
-export const { addFavourite, delitemarksFavourite } = postsSlice.actions;
+export const selectPosts = (state: any) => state.posts;
+
+export const { addFavourite, delitemarksFavourite, deliteAllFavourite } =
+  postsSlice.actions;
 export default postsSlice.reducer;
