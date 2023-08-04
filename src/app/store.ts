306729b28931e1a09223e843404351091d89a9ca
AnastasiyaKuksa64 <kuksa.nastya649@gmail.com>
2023-08-05 00:50:23 +0300

    added new reducers

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,19 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from "../appSlices/postsSlice";
-import SerchTermReducer from "../appSlices/SearchTermSlice";
 import ThemeReducer from "../appSlices/ThemeSlice";
 import SignInReducer from "../appSlices/SignInSlice";
-import filteredPostsReducer from "../appSlices/filterSlice";
+import filterReducer from "../appSlices/filterSlice";
 import SignUpReducer from "../appSlices/SignUpSlice";
 
 export const store = configureStore({
   reducer: {
     posts: postsReducer,
-    searchTerm: SerchTermReducer,
     Theme: ThemeReducer,
     User: SignInReducer,
     Registration: SignUpReducer,
-    filterPosts: filteredPostsReducer,
+    filterReducer,
   },
 });
 
